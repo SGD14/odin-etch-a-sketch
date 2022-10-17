7ac948869a10b37f98e1fb924f964cb88ae6fb1e
Dani <24252795+SGD14@users.noreply.github.com>
2022-10-17 14:41:50 +0200

    Add 16x16 grid

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let container = document.querySelector("#container");
+
+for(let row = 0; row < 16; row++){
+    let rowContainer = document.createElement("div");
+    rowContainer.classList.add("row");
+
+    for(let column = 0; column < 16; column++){
+        let columnContainer = document.createElement("div");
+        columnContainer.classList.add("column");
+
+        rowContainer.appendChild(columnContainer);
+    }
+
+    container.appendChild(rowContainer);
+}
