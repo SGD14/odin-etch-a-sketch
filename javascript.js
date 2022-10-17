c24b353c90c78360b69fc5f7606e8bc98d0f928d
Dani <24252795+SGD14@users.noreply.github.com>
2022-10-17 15:37:55 +0200

    Add new grid button behavior

--- javascript.js
+++ javascript.js
@@ -1,22 +1,31 @@
 let container = document.querySelector("#container");
 
-for(let row = 0; row < 16; row++){
-    let rowContainer = document.createElement("div");
-    rowContainer.classList.add("row");
+const generateGrid = (size) => {
+    container.innerHTML = "";
 
-    for(let column = 0; column < 16; column++){
-        let columnContainer = document.createElement("div");
-        columnContainer.classList.add("column");
-
-        columnContainer.addEventListener('mouseover', () => columnContainer.classList.add("colored"));
-
-        rowContainer.appendChild(columnContainer);
+    for(let row = 0; row < size; row++){
+        let rowContainer = document.createElement("div");
+        rowContainer.classList.add("row");
+    
+        for(let column = 0; column < size; column++){
+            let columnContainer = document.createElement("div");
+            columnContainer.classList.add("column");
+    
+            columnContainer.addEventListener('mouseover', () => columnContainer.classList.add("colored"));
+    
+            rowContainer.appendChild(columnContainer);
+        }
+    
+        container.appendChild(rowContainer);
     }
-
-    container.appendChild(rowContainer);
 }
+
+generateGrid(16);
 
 let newGridButton = document.querySelector("#new-grid-button");
 newGridButton.addEventListener('click', () => {
-    
+    let size = prompt("Select size (max 100)");
+
+    if(size <= 100)
+        generateGrid(size);
 })
